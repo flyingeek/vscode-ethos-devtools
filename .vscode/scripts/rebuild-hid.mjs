20f9fd0e2a1b6a349de65d435431d17df0a9dad8
Eric Delord <04-ete.elit@icloud.com>
2026-06-19 16:44:18 +0200

    fix: rebuild-hid.mjs path problem, remove security alerts for tar

--- .vscode/scripts/rebuild-hid.mjs
+++ .vscode/scripts/rebuild-hid.mjs
@@ -13,7 +13,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, '../..');
 
 /** Fetch a URL and return the body as a string. */
 function fetch(url) {
